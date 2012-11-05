0971b6b1e79ecdc25f274c53c905dec77be4957f
Natalia Lany <lany.natalia@gmail.com>
2012-11-05 09:42:52 +0100

    Pierwszy komit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Wypozyczalnia2::Application.config.secret_token = '864a36fe24df10fdf26f36ae429bfcf758e7040d4082a821fc55e632a81bf1476e793d2298a587b0cb0ca4fdc5da43a6fd89c9de4972128cec32295ff488a721'
